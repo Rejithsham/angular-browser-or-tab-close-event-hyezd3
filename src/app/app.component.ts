92379b85262e16431d289dea971c2b805a3ae3c4
Rejithsham <starrejith@gmail.com>
2020-10-09 17:52:06 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,14 @@ import { Component, NgModule, HostListener,Injectable  } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 
-  // Keep me Signed in
-  public doUnload(): void {
-    this.doBeforeUnload();
-  }
 
-  // Keep me Signed in
-  public doBeforeUnload(): void {
-    // Clear localStorage
-    localStorage.removeItem('username_key');
-  }
-
-  public storeData(txt): void {
-    localStorage.setItem('username_key', txt);
-  }
    @HostListener('window:beforeunload', ['$event'])
      public beforeunloadHandler($event) {
        console.log("data success");
        //return "Are you sure?";
+       /* In this Section we can use the remove local store as well as generate Server side  token deacttivating*/
        $event.returnValue = "Are you sure?";
-    /* $event.returnValue = "Are you sure?";
-     if($event.returnValue === true){
-       console.log("Api call Success");
-     }*/
+    
      
     }
 
