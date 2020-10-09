a5ffc901c509e2a5f2eeddc012973e713f7a719f
Rejithsham <starrejith@gmail.com>
2020-10-09 17:47:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-browser-or-tab-close-event-hyezd3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-browser-or-tab-close-event-hyezd3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, NgModule, HostListener,Injectable  } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  // Keep me Signed in
+  public doUnload(): void {
+    this.doBeforeUnload();
+  }
+
+  // Keep me Signed in
+  public doBeforeUnload(): void {
+    // Clear localStorage
+    localStorage.removeItem('username_key');
+  }
+
+  public storeData(txt): void {
+    localStorage.setItem('username_key', txt);
+  }
+   @HostListener('window:beforeunload', ['$event'])
+     public beforeunloadHandler($event) {
+       console.log("data success");
+       //return "Are you sure?";
+     $event.returnValue = "Are you sure?";
+     if($event.returnValue === true){
+       console.log("Api call Success");
+     }
+     
+    }
+
+}
+
